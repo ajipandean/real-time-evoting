f0f92ecb10ec7cd88c8e6dff3c62af32751dc6e7
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-07-09 15:58:54 +0800

    Add vote field in candidates model

--- app/models/candidate.js
+++ app/models/candidate.js
@@ -4,6 +4,11 @@ const candidateSchema = mongoose.Schema({
   president: String,
   vicePresident: String,
   vision: String,
+  vote: {
+    type: Number,
+    default: 0,
+    min: 0,
+  },
 });
 
 module.exports = mongoose.model('Candidate', candidateSchema);
